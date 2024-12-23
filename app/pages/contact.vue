b1eb1136a50905be5ae1381eabfb86266e613998
Alaics <acerlaptop0619@gmail.com>
2024-12-23 10:59:08 +0800

    ref: contact page

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        <h1 class="text-9xl">Contact Us</h1>
+        <!-- Hero Section -->
+        <div class="bg-white-900 w-screen">
+            <div class="flex items-center justify-center text-white h-[500px]">
+                <div class="text-center">
+                    <h1 class="font-bold text-[60px] leading-none uppercase">
+                        <span class="text-yellow-600">Connect</span>
+                        <span style="color: rgba(132, 77, 220, 1);"> with </span>
+                        <span style="color: rgba(0, 184, 212, 1);">us</span>
+                    </h1>
+                    <h2 class="text-black text-xl leading-none  mt-4">
+                        To Build Your Success Story
+                    </h2>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
