b737c9c63cc187fc938945b23a272a612a5044de
Jerald011003 <coninegaming@gmail.com>
2025-01-03 17:46:20 +0800

    feat latest page v2

--- app/pages/careers.vue
+++ app/pages/careers.vue
@@ -4,7 +4,7 @@
   >
     <!-- Main Content -->
     <div
-      class="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 py-20"
+      class="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 py-12"
     >
       <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold text-gray-900">
         Join us
