61809c47d824860d6fa88f36c6746835941cf6b7
Alaics <acerlaptop0619@gmail.com>
2025-01-13 11:17:49 +0800

    fix: srvices data

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -74,29 +74,97 @@
         </div>
 
         <!-- Services -->
-        <div class="space-y-6 col-span-1 w-full md:w-1/2">
+        <div class="space-y-6 col-span-1 w-full md:w-1/3">
           <div>
             <h3 class="font-medium mb-4">Outsourcing Services</h3>
             <div class="flex flex-col md:flex-row gap-x-20 gap-y-2">
               <ul class="text-sm text-gray-600">
-                <li>Virtual Assistant</li>
-                <li>Healthcare Virtual Assistants</li>
-                <li>Cloud Engineers</li>
-                <li>.Net API Developers</li>
-                <li>Digital Marketing & Creative</li>
-                <li>Customer Service Assistants</li>
-                <li>Accountants</li>
-                <li>Executive Assistants</li>
+                <li>
+                  <a href="/services/b59a2b98-75a1-4959-8b82-79e9ac92b30b" class="hover:text-black"
+                    >Virtual Assistant</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/healthcare-virtual-assistants"
+                    class="hover:text-black"
+                    >Healthcare Virtual Assistants</a
+                  >
+                </li>
+                <li>
+                  <a href="/cloud-engineers" class="hover:text-black"
+                    >Cloud Engineers</a
+                  >
+                </li>
+                <li>
+                  <a href="/net-api-developers" class="hover:text-black"
+                    >.Net API Developers</a
+                  >
+                </li>
+                <li>
+                  <a href="/digital-marketing-creative" class="hover:text-black"
+                    >Digital Marketing & Creative</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/customer-service-assistants"
+                    class="hover:text-black"
+                    >Customer Service Assistants</a
+                  >
+                </li>
+                <li>
+                  <a href="/accountants" class="hover:text-black"
+                    >Accountants</a
+                  >
+                </li>
+                <li>
+                  <a href="/executive-assistants" class="hover:text-black"
+                    >Executive Assistants</a
+                  >
+                </li>
               </ul>
               <ul class="text-sm text-gray-600">
-                <li>Software Engineers</li>
-                <li>Engineering</li>
-                <li>Front End Developers</li>
-                <li>Network Engineers</li>
-                <li>Real Estate Agents</li>
-                <li>Bookkeepers</li>
-                <li>Back-end Developers</li>
-                <li>And Many More...</li>
+                <li>
+                  <a href="/software-engineers" class="hover:text-black"
+                    >Software Engineers</a
+                  >
+                </li>
+                <li>
+                  <a href="/engineering" class="hover:text-black"
+                    >Engineering</a
+                  >
+                </li>
+                <li>
+                  <a href="/front-end-developers" class="hover:text-black"
+                    >Front End Developers</a
+                  >
+                </li>
+                <li>
+                  <a href="/network-engineers" class="hover:text-black"
+                    >Network Engineers</a
+                  >
+                </li>
+                <li>
+                  <a href="/real-estate-agents" class="hover:text-black"
+                    >Real Estate Agents</a
+                  >
+                </li>
+                <li>
+                  <a href="/bookkeepers" class="hover:text-black"
+                    >Bookkeepers</a
+                  >
+                </li>
+                <li>
+                  <a href="/back-end-developers" class="hover:text-black"
+                    >Back-end Developers</a
+                  >
+                </li>
+                <li>
+                  <a href="/and-many-more" class="hover:text-black"
+                    >And Many More...</a
+                  >
+                </li>
               </ul>
             </div>
             <div class="mt-4 w-full md:w-1/2">
@@ -148,14 +216,91 @@
               </p>
             </div>
           </div>
+          <div>
+            <h3 class="font-medium">USA</h3>
+            <div
+              class="flex items-center justify-center md:justify-start space-x-2"
+            >
+              <svg
+                class="icon-fixed-size"
+                viewBox="0 0 24 24"
+                fill="black"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+                />
+              </svg>
+              <p class="text-sm text-gray-600">+1 207 241-5887</p>
+            </div>
+            <div
+              class="flex items-center justify-center md:justify-start space-x-2 mt-2"
+            >
+              <svg
+                class="icon-fixed-size"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
+                <circle cx="12" cy="10" r="3" />
+              </svg>
+              <p class="text-sm text-gray-600">
+                1150 NW 72nd Ave Tower 1 Ste 455 #14500, Miami FL 33126, US.
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 class="font-medium">USA</h3>
+            <div
+              class="flex items-center justify-center md:justify-start space-x-2"
+            >
+              <svg
+                class="icon-fixed-size"
+                viewBox="0 0 24 24"
+                fill="black"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+                />
+              </svg>
+              <p class="text-sm text-gray-600">+1 207 241-5887</p>
+            </div>
+            <div
+              class="flex items-center justify-center md:justify-start space-x-2 mt-2"
+            >
+              <svg
+                class="icon-fixed-size"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
+                <circle cx="12" cy="10" r="3" />
+              </svg>
+              <p class="text-sm text-gray-600">
+                1150 NW 72nd Ave Tower 1 Ste 455 #14500, Miami FL 33126, US.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <!-- Certifications -->
       <div class="-mt-4 flex flex-wrap justify-center items-center gap-8">
-        <AnimatedLogoCloud
-        :logos
-        title="Certified Professionals"
-      />
+        <AnimatedLogoCloud :logos title="Certified Professionals" />
       </div>
     </div>
   </footer>
@@ -165,13 +310,17 @@
 import sanityClient from "~/hooks/sanityClient";
 import { urlFor } from "~/hooks/sanityImageUrl";
 import type { Footer } from "~/types/footer";
-import AnimatedLogoCloud from '@/components/AnimateLogo/AnimatedLogoCloud.vue';
+import AnimatedLogoCloud from "@/components/AnimateLogo/AnimatedLogoCloud.vue";
 
 const footer = ref<Footer[]>([]);
 
 const logo = ref<{ path: string }[]>([]);
 
-const imageUrl = computed(() => footer.value.length > 0 && footer.value[0].images ? urlFor(footer.value[0].images[0]) : '');
+const imageUrl = computed(() =>
+  footer.value.length > 0 && footer.value[0].images
+    ? urlFor(footer.value[0].images[0])
+    : ""
+);
 
 const logos = ref([
   {
@@ -193,7 +342,7 @@ const logos = ref([
   {
     name: "Airbnb",
     path: "",
-  }
+  },
 ]);
 
 onMounted(async () => {
@@ -211,7 +360,6 @@ onMounted(async () => {
     console.error("Error fetching data from Sanity:", error);
   }
 });
-
 </script>
 
 <style scoped>
---
+++ app/types/servicesData.ts
@@ -0,0 +1,25 @@
+interface sections {
+    description: string;
+    miniTitle: string;
+    _key: string;
+  }
+  export interface ServicesData {
+    bodyTitle:{
+      sections: sections[];
+    }
+    collabDescription: string;
+    collabTitle: string;
+    heading: string;
+    icon: any
+    id: string;
+    introDescription: string;
+    introTitle: string;
+    image: any;
+    subheading: string;
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
+  }
+  
