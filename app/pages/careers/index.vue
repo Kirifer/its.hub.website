e86a2434e7c90628ea9f00109b05c40abc87fda0
Alaics <acerlaptop0619@gmail.com>
2025-01-15 12:02:02 +0800

    fix: carreer hero image and button router link

--- app/pages/careers/index.vue
+++ app/pages/careers/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div
-  class=" animate-fade-grid-in-2 inset-0 overflow-x-hidden overflow-y-hidden w-full bg-white bg-[linear-gradient(to_right,#80808012_3px,transparent_1px),linear-gradient(to_bottom,#80808012_3px,transparent_1px)] bg-[size:100px_100px]"
+    class="animate-fade-grid-in-2 inset-0 overflow-x-hidden overflow-y-hidden w-full bg-white bg-[linear-gradient(to_right,#80808012_3px,transparent_1px),linear-gradient(to_bottom,#80808012_3px,transparent_1px)] bg-[size:100px_100px]"
   >
     <!-- Main Content -->
     <div
-    
-      class="z-10 flex flex-col items-center justify-center min-h-screen px-4 py-12 "
+      class="z-10 flex flex-col items-center justify-center min-h-screen px-4 py-12"
     >
-      <div v-if="careers.length > 0"  class="relative animate-fade-in ">
+      <div v-if="careers.length > 0" class="relative animate-fade-in">
         <div class="overflow-hidden h-full z-0">
           <div
             class="absolute inset-0 h-[500px] flex items-center justify-center"
@@ -46,19 +45,23 @@
             {{ careers[0]?.hero_subheading }}
           </p>
         </div>
-        <div class="relative z-10 flex justify-center mt-7" @click="scrollToJobOpenings">
-           <ShimmerButton
-               shimmer-size="2px"
-              class="bg-[rgba(132,77,220,0.9)] text-white font-medium px-5 py-1.5 rounded-md hover:bg-purple-300 hover:text-white transition-colors"              >
-              {{ careers[0]?.hero_button }}
-              </ShimmerButton>
-</div>
+        <div
+          class="relative z-10 flex justify-center mt-7"
+          @click="scrollToJobOpenings"
+        >
+          <ShimmerButton
+            shimmer-size="2px"
+            class="bg-[rgba(132,77,220,0.9)] text-white font-medium px-5 py-1.5 rounded-md hover:bg-purple-300 hover:text-white transition-colors"
+          >
+            {{ careers[0]?.hero_button }}
+          </ShimmerButton>
+        </div>
         <div
           class="relative z-10 mt-8 p-3 rounded-2xl bg-gradient-to-r from-purple-400 via-blue-400 to-purple-400 max-w-4xl w-full mx-auto"
         >
           <div class="rounded-xl overflow-hidden">
             <img
-              src="~/assets/images/its-hiring.jpg"
+              :src="careers[0]?.hero_image || '~/assets/images/its-hiring.jpg'"
               alt="Two people in a business meeting"
               class="w-full h-[200px] sm:h-[300px] md:h-[450px] object-cover"
             />
@@ -66,7 +69,7 @@
         </div>
         <!-- Cyan gradient Right-->
         <div
-          class="absolute  md:h-[686px] md:w-[682px] w-[282px] h-[286px] rounded-full right-[-250px] top-48 z-0"
+          class="absolute md:h-[686px] md:w-[682px] w-[282px] h-[286px] rounded-full right-[-250px] top-48 z-0"
           :style="{
             background:
               'radial-gradient(circle, rgba(142,212,246,0.3) 0%, rgba(142,212,246,0) 50%)',
@@ -107,7 +110,7 @@
       </div>
 
       <!-- Explore Our Job Openings -->
-      <div class="container px-2 pt-12 z-10 "  ref="jobOpenings">
+      <div class="container px-2 pt-12 z-10" ref="jobOpenings">
         <div class="sm:p-6 md:p-8 z-10">
           <div class="mx-auto space-y-4 sm:space-y-6 md:space-y-6">
             <h2
@@ -143,7 +146,7 @@
             </div>
 
             <!-- Job Cards Container -->
-            <div 
+            <div
               class="mt-8 sm:mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8"
             >
               <div
@@ -153,13 +156,14 @@
               >
                 <!-- Available Badge -->
                 <div class="space-y-4">
-                  
                   <div class="flex items-center space-x-2">
-  <CheckIcon class="w-5 h-5 text-purple-900" />
-  <span class="inline-block text-lg sm:text-xl font-medium text-violet-600">
-    Available
-  </span>
-</div>
+                    <CheckIcon class="w-5 h-5 text-purple-900" />
+                    <span
+                      class="inline-block text-lg sm:text-xl font-medium text-violet-600"
+                    >
+                      Available
+                    </span>
+                  </div>
 
                   <!-- Job Title -->
                   <h3 class="font-semibold text-xl sm:text-2xl text-gray-900">
@@ -179,8 +183,8 @@
                   <NuxtLink
                     :to="`/careers/${card._id}`"
                     class="px-4 py-2 text-sm md:text-base font-semibold bg-purple-200 text-purple-800 rounded-md hover:bg-purple-300 transition-all duration-300 hover:scale-105"
-                    >
-                 Learn More
+                  >
+                    Learn More
                   </NuxtLink>
                 </div>
               </div>
@@ -209,21 +213,22 @@
         </div>
 
         <div class="justify-center text-center space-y-8 mt-16 mb-16">
-          <h2 class="text-2xl sm:text-4xl md:text-5xl font-bold text-gray-900">
+          <h2
+            class="text-2xl sm:text-4xl md:text-5xl font-bold text-gray-900 mb-4 sm:mb-6"
+          >
             {{ careers[0]?.section2_heading }}
           </h2>
-
-          <button
-          class="px-4 py-2 text-sm md:text-base font-semibold bg-purple-200 text-purple-800 rounded-md hover:bg-purple-300 transition-all duration-300 hover:scale-105"
-          >
-            {{ careers[0]?.section2_button }}
-          </button>
+          <router-link to="/services">
+            <button
+              class="px-4 py-2 text-sm md:text-base font-semibold bg-purple-200 text-purple-800 rounded-md hover:bg-purple-300 transition-all duration-300 hover:scale-105"
+            >
+              {{ careers[0]?.section2_button }}
+            </button>
+          </router-link>
         </div>
       </div>
     </div>
   </div>
-
-  
 </template>
 
 <script setup lang="ts">
@@ -249,9 +254,7 @@ const filteredAndSearchedCards = computed(() => {
 
   return careers.value[0].section1_cards.filter((card) => {
     const matchesSearch =
-      card.job_title
-        .toLowerCase()
-        .includes(searchQuery.value.toLowerCase()) ||
+      card.job_title.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
       card.job_description
         .toLowerCase()
         .includes(searchQuery.value.toLowerCase());
@@ -290,8 +293,7 @@ const navigate = (direction: number) => {
 onMounted(async () => {
   try {
     careers.value = await sanityClient.fetch<Careers[]>(
-      '*[_type == "careers"]{..., "section1_cards": section1_cards[]->}'
-     
+      '*[_type == "careers"]{..., "section1_cards": section1_cards[]->, "hero_image": hero_image.asset->url}'
     );
     console.log(careers);
   } catch (error) {
@@ -313,7 +315,6 @@ onMounted(async () => {
     opacity: 1;
   }
 }
-
 
 .animate-fade-in-2ndcontainer {
   animation: fade-in-2ndcontainer 3s ease;
@@ -342,5 +343,4 @@ onMounted(async () => {
     transform: translateY(0);
   }
 }
-
 </style>
